2c9f2a86e5f765264b09f336f85706af5eee0fa2
Andy Makovec <andymakovec@gmail.com>
2021-07-07 22:06:43 -0400

    feat: Added check to see if Launchpad version is newer than AUR version.  If check fails, PKGBUILD update isn't performed.

---
+++ aur.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_aur_version():
+    response = requests.get('https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=kicad-nightly-bin')
+    json_data = response.json()
+    package_version = json_data['results'][0]['Version']
+    package_version = package_version.split('-')[0]
+    return package_version
+
+if __name__ == '__main__':
+    version = get_aur_version()
+    print(version)
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from get_latest_binary import get_latest_binary
 from pkgbuild import pkgbuild_gen
 from git import clone_from_aur, local_commit, push_to_aur, keyfile_from_heroku_configvar
+from aur import get_aur_version
+import sys
 
 if __name__ == '__main__':
-    version, binary_url = get_latest_binary()
-    pkgbuild_gen(version, binary_url, 'kicad-nightly.env')
+    launchpad_version, binary_url = get_latest_binary()
+    aur_version = get_aur_version()
+
+    if aur_version == launchpad_version:
+        sys.exit()
+
+    pkgbuild_gen(launchpad_version, binary_url, 'kicad-nightly.env')
 
     keyfile_from_heroku_configvar()
     repo = clone_from_aur()
-    local_commit(version, repo)
+    local_commit(launchpad_version, repo)
     push_to_aur(repo)
